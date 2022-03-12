a79bb81d0fd88d3f8478d3b0025d312b0f9b8ede
Juan Carlo Castillo <juancarlo.castillo20@gmail.com>
2022-03-12 15:35:39 +0100

    sprint 3 nivell 2

---
+++ nivell2/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sprint 3 nivell 2</title>
+        <style>
+                h1 , h2 {
+                        text-align : center;
+                }
+                .ex {
+                        padding: 20px;
+                        background-color: #6fa8dc;
+                }
+        </style>
+</head>
+<body>
+        <h1>Adapter</h1>
+        <h2>Nivell 2</h2>
+<section class="ej02 ex">
+<?php
+echo "hola";
+
+// echo "<h3> Exercici 2 : Adapter  </h3>
+//         <p>Suposeu que té les següents dues classes de PHP.</p>";
+// echo "<p> class Duck { <br/>
+//        public function quack() {<br/>
+//               echo 'Quack \n';<br/>
+//        }<br/>
+//        public function fly() {<br/>
+//               echo 'I´m flying \n';<br/>
+//        }<br/>
+//    }<br/></p>";
+// echo "<p> class Turkey {<br/>
+//        public function gobble() {<br/>
+//               echo 'Gobble gobble \n';<br/>
+//        }<br/>
+//        public function fly() {<br/>
+//        echo 'I´m flying a short distance \n';<br/>
+//        }
+//    }<br/></p>";
+// echo "<p> Voleu que els seus galls d'indi es comportin com ànecs, de manera que ha d'aplicar el adapter pattern. En el mateix arxiu, escriviu una classe anomenada TurkeyAdapter i assegureu-vos que tingui en compte el següent:</p>";
+// echo "<p> La traducció de l'quack entre classes és fàcil: simplement cridi al mètode Gobble quan sigui apropiat. </p>";
+// echo "<p> Encara que ambdues classes tenen un mètode fly, els galls dindis només poden volar a ratxes curtes, no poden volar llargues distàncies com els ànecs. Per mapejar entre el mètode fly d'un ànec i el mètode del gall dindi, ha de trucar a l'mètode fly del gall dindi cinc vegades per compensar-ho.</p>";
+// echo "<p> Prova la teva classe amb el següent codi:
+// </p>";
+// echo '<p> function duck_interaction($duck){<br/>
+//        $duck->quack();<br/>
+//        $duck->fly();<br/>
+// }<br/></p>";
+// echo "<p> $duck = new Duck;<br/>
+// $turkey = new Turkey;<br/>
+// $turkey_adapter = new TurkeyAdapter($turkey);<br/>
+// echo "The Turkey says...\n";<br/>
+// $turkey->gobble();<br/>
+// $turkey->fly();<br/>
+// echo "The Duck says...\n";<br/>
+// duck_interaction($duck);<br/>
+// echo "The TurkeyAdapter says...\n";<br/>
+// duck_interaction($turkey_adapter);</p>';
+// echo '<p> Output<br/>
+// The expected output is as follows:<br/>
+// The Turkey says...<br/>
+// Gobble gobble<br/>
+// I´m flying a short distance<br/>
+// The Duck says...<br/>
+// Quack<br/>
+// I´m flying<br/>
+// The TurkeyAdapter says...<br/>
+// Gobble gobble<br/>
+// I´m flying a short distance<br/>
+// I´m flying a short distance<br/>
+// I´m flying a short distance<br/>
+// I´m flying a short distance<br/>
+// I´m flying a short distance</p>';
+// echo "<p><strong>Resultado:</strong></p>";
+
+
+
+// class Duck {
+//     public function quack() {
+//            echo "Quack \n";
+//     }
+//     public function fly() {
+//            echo "I'm flying \n";
+//     }
+// }
+
+
+// class Turkey {
+
+//     public function gobble() {
+//            echo "Gobble gobble \n";
+//     }
+
+//     public function fly() {
+//     echo "I'm flying a short distance \n";
+//     }
+// }
+
+
+
+?>
+
+</section>
+        
+</body>
+</html>
